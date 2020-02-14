6c25f9049c66057b9f4c2fd711838de1ff9e70f9
Rajendra Somase <raj.somase@gmail.com>
2020-02-15 00:52:34 +0530

    basic and variables

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,17 @@
+var firstName = 'Rajendra'
+console.log(firstName);
+
+var lastName = 'Somase'
+console.log(lastName);
+var age = 35;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Software Engineer';
+console.log(job);
+
+var 2years;
