b33c72a48a76a98e1a0e829658fedcd02ebb72fe
Rajendra Somase <raj.somase@gmail.com>
2020-02-24 01:58:21 +0530

    5-Advance JS & 6-Budget Project

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -91,7 +91,7 @@ console.log(adult);*/
 
 //Functions Returning Functions
 
-function interviewQuestions(job){
+/*function interviewQuestions(job){
     if(job === 'Designer'){
         return function(name){
             console.log('Hey '+name + 'Could you please explain what is UX ?')
@@ -110,4 +110,138 @@ function interviewQuestions(job){
 var designerQuestion = interviewQuestions('Designer');
 designerQuestion('John');
 interviewQuestions('Teacher')('Jane');
-interviewQuestions('Driver')('Mark');
+interviewQuestions('Driver')('Mark');*/
+
+//Immediately invoked Functions for data hiding
+
+/*
+(function (){
+ var score = Math.random() *5;
+console.log(score);
+ })();
+
+(function(targate){
+    var score = Math.random()*5;
+    console.log(score >= targate)
+})(3);
+*/
+ 
+
+// Closure
+
+/*
+function interviewQuestions(job){
+    return function(name){
+    if(job === 'Designer'){
+        return  console.log('Hey '+name + ' Could you please explain what is UX ?');
+    }else if (job === 'Teacher'){
+        return console.log('What do you teach ' + name +' ?');
+        
+    }else{
+        return console.log('What do you do ? '+name);
+        }   
+    } 
+
+}
+
+interviewQuestions('Designer')('John');
+interviewQuestions('Teacher')('Marry');
+interviewQuestions('Driver')('Joseph');
+*/
+
+
+// Bind, Call & apply
+
+/*
+var john = {
+    name : 'John',
+    age : 30,
+    job : 'Designer',
+    presentation: function(style,timeofDay){
+        if(style === 'formal'){
+            console.log('Good '+timeofDay+ ' ladies & gentlements! I\'m '+this.name 
+                        + '. I\'m a '+this.job+'.'
+                       +' and I \'m '+this.age+ ' years old.');
+        }else if (style === 'Casual'){
+            console.log('Hey whats up guys, I\'m '+this.name 
+                        + '. I\'m a '+this.job+'.'
+                       +' and I \'m '+this.age+ ' years old.');
+        }
+    }
+}
+
+john.presentation('formal','Morning');
+john.presentation('Casual','Evening');
+
+var emily ={
+    name: 'Emily',
+    age:22,
+    job:'Teacher'
+
+}
+
+//john.presentation.call(emily,'Casual','Morning');
+//john.presentation.apply(emily,['Casual','Morning']);
+john.presentation.bind(emily,'Casual')('Morning');
+
+
+
+var years = [1990,1955,2014,1984,1963,2019];
+
+function calculateArray(array,fn){
+    returnResult = []
+    for(var i =0; i<array.length;i++){
+        returnResult.push(fn(array[i]));
+    }
+    return returnResult;
+}
+
+function calculateAge(year){
+    return 2020 - year;
+}
+
+var ages = calculateArray(years,calculateAge);
+
+
+function isAdult(limit,age){
+    return age >= limit ? true : false;
+}
+
+var adult = calculateArray(ages,isAdult.bind(this,35));
+console.log(ages);
+console.log(adult);
+*/
+//Bind is used to preset arguments to method
+
+
+/*******************************************************************
+*  CODING CHALLANGE
+*/
+
+var Question = function (question,answers,correctAnswer){
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+    
+}
+
+questionCourseTutor = new Question('Who is Tutor for this courese ?',['John','Marry','Jonas'],2);
+questionCourseSubject = new Question('What is this courese about ?',['Java','PHP','JavaScript'],2);
+questionTodaysDate = new Question('What is todays date ?',['22 Feb 2020','20 Feb 2019','20 Feb 2018'],0)
+
+var questions = [questionCourseTutor,questionCourseSubject,questionTodaysDate];
+
+
+(function(){
+  for(var i =0;i<questions.length;i++){
+    var randomNumber = Math.floor(Math.random() * 3);   
+      console.log(randomNumber);
+     console.log('Question : '+questions[randomNumber].question)
+      console.log('0 : '+questions[randomNumber].answers[0]);
+      console.log('1 : '+questions[randomNumber].answers[1]);
+      console.log('2 : '+questions[randomNumber].answers[2]);
+      var answer = prompt(questions[randomNumber].question);
+      console.log(answer == questions[i].correctAnswer?'You are correct.':'Wrong Answer.')
+  }
+
+})();
---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,118 @@
+var budgetController = (function(){
+    
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var Expenses = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var testExpense = new Expenses(1,'TestExpense',10);
+    var testIncome = new Income(1,'TestIncome',10);
+    console.log(testExpense,testIncome);
+    
+    var budgetData = {
+        allItems : {
+            exp:[],
+            inc:[]
+        },
+        totals: {
+            exp:0,
+            inc:0
+        }
+    }
+    
+    return {
+        addItem: function(type, description,value){
+            var ID, newItem;
+            //ID
+            
+            if(budgetData.allItems[type].length>0){
+                ID = budgetData.allItems[type][budgetData.allItems[type].length - 1].id +1;    
+            }else{
+                ID = 0;    
+            }
+            
+            
+            
+            //New Item
+            if(type === 'exp'){
+                newItem = new Expenses(ID,description,value);   
+            }else if(type === 'inc'){
+                newItem = new Income(ID,description,value);
+            }
+            
+            //Add Item to Data
+            budgetData.allItems[type].push(newItem);
+            console.log(budgetData.allItems[type]);
+            return newItem;
+        }
+    }
+        
+})();
+
+var UIController = (function(){
+    
+    var DOMString = {
+        DOMType: '.add__type',
+        DOMDescription : '.add__description',
+        DOMValue : '.add__value',
+        DOMAddbtn : '.add__btn'
+    }
+    return {
+        getInput: function(){
+            return {
+                type : document.querySelector(DOMString.DOMType).value,
+                description : document.querySelector(DOMString.DOMDescription).value,
+                value : document.querySelector(DOMString.DOMValue).value
+            };
+        },
+        
+        getDOMString: function(){
+            return DOMString;
+        }
+    }
+})();
+
+
+var appController = (function(budgetController,UIController){
+    
+    
+    //All Event Listners are here
+    var eventListener = function(){
+        var DOMString = UIController.getDOMString();
+        
+        document.querySelector(DOMString.DOMAddbtn).addEventListener('click',appcnrlAddItems);
+        document.addEventListener('keypress',function(event){
+        if(event.keyCode === 13){
+                appcnrlAddItems();
+            }
+        });    
+    }
+    
+    // Add Items inputs is here
+    var appcnrlAddItems = function(){
+        //1. Read Input values
+        var input = UIController.getInput(); 
+        console.log(input);
+        
+        //2. Add Item
+        var addItem = budgetController.addItem(input.type,input.description,input.value);
+        console.log(addItem);
+    }     
+    
+    return {
+        init : function(){
+            console.log('Application started....')
+            eventListener();
+        }
+    }
+        
+})(budgetController,UIController);
+
+appController.init();
